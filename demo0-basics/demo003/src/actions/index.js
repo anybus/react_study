f6751bc10daa508795e02581afb90693f994d8c0
anybus <anybus@163.com>
2018-04-11 20:30:36 +0800

    todos

--- demo0-basics/demo003/src/actions/index.js
+++ demo0-basics/demo003/src/actions/index.js
@@ -1,22 +1,33 @@
 let nextTodoId = 0
-export const addTodo = text => ({
-  type: 'ADD_TODO',
-  id: nextTodoId++,
-  text
-})
 
-export const setVisibilityFilter = filter => ({
-  type: 'SET_VISIBILITY_FILTER',
-  filter
-})
-
-export const toggleTodo = id => ({
-  type: 'TOGGLE_TODO',
-  id
-})
-
-export const VisibilityFilters = {
-  SHOW_ALL: 'SHOW_ALL',
-  SHOW_COMPLETED: 'SHOW_COMPLETED',
-  SHOW_ACTIVE: 'SHOW_ACTIVE'
+function addTodo(text) {
+    return {
+        type: 'ADD_TODO',
+        id: nextTodoId++,
+        text: text
+    }
 }
+
+function setVisibilityFilter(filter) {
+    return {
+        type: 'SET_VISIBILITY_FILTER',
+        filter
+    }
+}
+
+
+function toggleTodo(id) {
+    return {
+        type: 'TOGGLE_TODO',
+        id
+    }
+}
+
+
+const VisibilityFilters = {
+    SHOW_ALL: 'SHOW_ALL',
+    SHOW_COMPLETED: 'SHOW_COMPLETED',
+    SHOW_ACTIVE: 'SHOW_ACTIVE'
+};
+
+export {addTodo, setVisibilityFilter, toggleTodo, VisibilityFilters}
--- demo0-basics/demo003/src/components/App.js
+++ demo0-basics/demo003/src/components/App.js
@@ -1,14 +1,10 @@
 import React from 'react'
-import Footer from './Footer'
 import AddTodo from '../containers/AddTodo'
-import VisibleTodoList from '../containers/VisibleTodoList'
 
 const App = () => (
-  <div>
-    <AddTodo />
-    <VisibleTodoList />
-    <Footer />
-  </div>
-)
+    <div>
+        <AddTodo/>
+    </div>
+);
 
 export default App
